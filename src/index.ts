2f5ee766c9e8d539a365ed170d6444ced43e4aed
Lucas Duarte <lucasdu4rte@gmail.com>
2021-10-16 01:15:42 -0300

    feature: add function secondsToTimeDuration

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,13 @@ export const timeDurationToSeconds = (time: string): number => {
 
   return (+hh) * 3600 + (+mm) * 60 + (+ss)
 }
+
+export const secondsToTimeDuration = (seconds: number): string => {
+  return [3600, 60]
+    .reduceRight(
+      (p, b) => r => [Math.floor(r / b)].concat(p(r % b)),
+      (r:number) => [r]
+    )(seconds)
+    .map(a => a.toString().padStart(2, '0'))
+    .join(':');
+}
